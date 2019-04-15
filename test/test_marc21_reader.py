8f326fa205e69c8571317e707c2394cac8690117
Tobias Schweizer <t.schweizer@unibas.ch>
2019-04-15 18:44:21 +0200

    test (test for author): add test for author field

--- test/test_marc21_reader.py
+++ test/test_marc21_reader.py
@@ -1,7 +1,17 @@
+import unittest
+
 from Marc21_reader import marc21_reader
 
-marc21 = marc21_reader.get_marc21('sample_data/000055275.marc')
-records = marc21_reader.get_records(marc21)
+class TestStringMethods(unittest.TestCase):
 
-author = marc21_reader.get_author(records)
-print(author)
+    def test_author(self):
+        marc21 = marc21_reader.get_marc21('sample_data/000055275.marc')
+        records = marc21_reader.get_records(marc21)
+
+        author = marc21_reader.get_author(records)
+
+        self.assertEqual(len(author), 1)
+
+        self.assertEqual(author[0]['name'], unicode('Bernoulli, Daniel,'))
+        self.assertEqual(author[0]['date'], unicode('1700-1782'))
+        self.assertEqual(author[0]['GND'], unicode('(DE-588)118656503'))
