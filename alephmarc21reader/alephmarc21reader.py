b6a9a8d274c410d207495691a2926fc1c4305f67
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2019-04-17 18:23:06 +0200

    Wip/add methods (#2)
    * refactor (person): improve consistency
    
    * feature (recipient): add method to get the recipient
    
    * feature (mentioned): add method to get the mentioned persons
    
    * refactor (person): check for subfield
    
    * feature (date): add method to get the date
    
    * feature (creation place): add method to get the creation place
    
    * feature (shelfmark): add method to get the shelfmark
    
    * feature (footnote): add method to get the footnote
    
    * feature (footnote): add docstring

--- alephmarc21reader/alephmarc21reader.py
+++ alephmarc21reader/alephmarc21reader.py
@@ -51,10 +51,10 @@ class AlephMarc21Reader:
     class Person:
         """
         Represents a person.
-        :param str name: the name of the person (family name, first name).
-        :param str lifespan: the lifespan of the person (year of birth and death separated by a '-').
-        :param str gnd: the GND of the person, otherwise 'no_GND'.
-        :param str role: the role of the person (author etc.).
+        :param str|False name: the name of the person (family name, first name), if any.
+        :param str|False lifespan: the lifespan of the person (year of birth and death separated by a '-'), if any.
+        :param str|False gnd: the GND of the person, otherwise 'no_GND', if any.
+        :param str|False role: the role of the person (author etc.), if any.
 
         """
 
@@ -73,35 +73,40 @@ class AlephMarc21Reader:
         :return: Person
         """
 
-        if marc_field['d'] is not None:
+        if 'a' in marc_field:
+            name = marc_field['a']
+        else:
+            name = False
+
+        if 'd' in marc_field:
             date = marc_field['d']
         else:
-            date = 'no_date'
+            date = False
 
-        if marc_field[gnd_index] is None:
-            GND = 'no_GND'
-        else:
+        if gnd_index in marc_field:
             # get rid of trailing comma
-            GND = marc_field[gnd_index].replace(',', '')
+            gnd = marc_field[gnd_index].replace(',', '')
+        else:
+            gnd = False
 
-        if marc_field['4'] is not None:
+        if '4' in marc_field:
             role = marc_field['4']
         else:
-            role = 'no_role'
+            role = False
 
-        return AlephMarc21Reader.Person(marc_field['a'], date, GND, role)
+        return AlephMarc21Reader.Person(name, date, gnd, role)
     __get_person_info.__annotations__ = {'marc_field': pymarc.field.Field, 'gnd_index': str, 'return': Person}
 
     def get_author(self):
         """
-        Returns author information from a Marc21 record.
-        :return: Person
+        Returns information about the author.
+        :return: [Person]
         """
         author = []
         for field in self.record.get_fields('100'):
             author.append(self.__get_person_info(field, '0'))
 
-        # check for 700 that are actually authors
+        # check for recipients (700) that are actually authors
         for field in self.record.get_fields('700'):
             person = self.__get_person_info(field, '0')
 
@@ -109,4 +114,122 @@ class AlephMarc21Reader:
                 author.append(person)
 
         return author
-    get_author.__annotations__ = {'return': Person}
+    get_author.__annotations__ = {'return': [Person]}
+
+    def get_recipient(self):
+        """
+        Returns information about the recipient.
+        :return: [Person]
+        """
+        recipient = []
+        for field in self.record.get_fields('700'):
+            person = self.__get_person_info(field, '0')
+
+            if person.role == "rcp":
+                recipient.append(person)
+
+        return recipient
+    get_recipient.__annotations__ = {'return': [Person]}
+
+    def get_mentioned_person(self):
+        """
+        Returns information about a mentioned person.
+        :return: [Person]
+        """
+        recipient = []
+        for field in self.record.get_fields('600'):
+            recipient.append(self.__get_person_info(field, '0'))
+
+        return recipient
+    get_mentioned_person.__annotations__ = {'return': [Person]}
+
+    def get_date(self):
+        """
+        Returns the date.
+        :return: [str]
+        """
+        date = []
+        for field in self.record.get_fields('046'):
+            if 'c' in field:
+                date.append(field['c'])
+
+        return date
+    get_date.__annotations__ = {'return': [str]}
+
+    class Place:
+        """
+        Represents a place.
+        :param str|False name: the name of the place, if any.
+        :param str|False gnd: the GND of the place, otherwise 'no_GND', if any.
+        """
+
+        def __init__(self, name, gnd):
+            self.name = name
+            self.gnd = gnd
+
+    def get_creation_place(self):
+        """
+        Returns the place of creation.
+        :return: [Place]
+        """
+        creation_place = []
+        for field in self.record.get_fields('751'):
+            if 'a' in field:
+                name = field['a']
+            else:
+                name = False
+
+            if '0' in field:
+                gnd = field['0']
+            else:
+                gnd = False
+
+            cp = AlephMarc21Reader.Place(name, gnd)
+            creation_place.append(cp)
+        return creation_place
+    get_creation_place.__annotations__ = {'return': [Place]}
+
+    class Shelfmark:
+        """
+        Represents a shelfmark.
+        :param str|False institution: the name of the institution, if any.
+        :param str|False identifier: the identifier, if any.
+        """
+        def __init__(self, institution, identifier):
+            self.institution = institution
+            self.identifier = identifier
+
+    def get_shelfmark(self):
+        """
+        Returns the shelfmark.
+        :return: [Shelfmark]
+        """
+        shelfmark = []
+        for field in self.record.get_fields('852'):
+            if 'a' in field:
+                institution = field['a']
+            else:
+                institution = False
+
+            if 'p' in field:
+                identifier = field['p']
+            else:
+                identifier = False
+
+            sm = AlephMarc21Reader.Shelfmark(institution, identifier)
+            shelfmark.append(sm)
+        return shelfmark
+    get_shelfmark.__annotations__ = {'return': [Shelfmark]}
+
+    def get_footnote(self):
+        """
+        Returns the footnote.
+        :return: [str]
+        """
+        footnote = []
+        for field in self.record.get_fields('500'):
+            if 'a' in field:
+                footnote.append(field['a'])
+
+        return footnote
+    get_footnote.__annotations__ = {'return': [str]}
--- alephmarc21reader/tests/test_Marc21Reader.py
+++ alephmarc21reader/tests/test_Marc21Reader.py
@@ -15,3 +15,68 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(author[0].lifespan, u'1700-1782')
         self.assertEqual(author[0].gnd, u'(DE-588)118656503')
         self.assertEqual(author[0].role, u'aut')
+
+    def test_recipient(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        recipient = marc21_rd.get_recipient()
+
+        self.assertEqual(len(recipient), 1)
+
+        self.assertEqual(recipient[0].name, u'Scheuchzer, Johann,')
+        self.assertEqual(recipient[0].lifespan, u'1684-1738')
+        self.assertEqual(recipient[0].gnd, u'(DE-588)120379260')
+        self.assertEqual(recipient[0].role, u'rcp')
+
+    def test_mentioned_person(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        mentioned = marc21_rd.get_mentioned_person()
+
+        self.assertEqual(len(mentioned), 6)
+
+        self.assertEqual(mentioned[0].name, u'Bernoulli, Johann')
+        self.assertEqual(mentioned[0].lifespan, u'1667-1748')
+        self.assertEqual(mentioned[0].gnd, u'(DE-588)118509969')
+        self.assertEqual(mentioned[0].role, False)
+
+    def test_date(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        date = marc21_rd.get_date()
+
+        self.assertEqual(len(date), 1)
+
+        self.assertEqual(date[0], u'1734.03.12')
+
+    def test_creation_place(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        cp = marc21_rd.get_creation_place()
+
+        self.assertEqual(len(cp), 1)
+
+        self.assertEqual(cp[0].name, u'Basel')
+        self.assertEqual(cp[0].gnd, u'(DE-588)4004617-5')
+
+    def test_get_shelfmark(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        sm = marc21_rd.get_shelfmark()
+
+        self.assertEqual(len(sm), 1)
+
+        self.assertEqual(sm[0].institution, u'ZB Z\xfcrich')
+        self.assertEqual(sm[0].identifier, u'Ms H 340, pp. 569-572')
+
+    def test_get_footnote(self):
+        marc21_rd = AlephMarc21Reader('alephmarc21reader/tests/sample_data/000055275.marc')
+
+        footnote = marc21_rd.get_footnote()
+
+        self.assertEqual(len(footnote), 1)
+
+        self.assertEqual(footnote[0], u'Die Briefhandschrift findet sich in einem Z\xfcricher Briefband mit der Aufschrift "Epistolae Helvetorum ad J. J. Scheuchzer". Johann Jakob Scheuchzer starb jedoch am 23.6.1733. Daniel Bernoulli hatte von dessen Tod sp\xe4testens im August 1733 auf der R\xfcckreise von St. Petersburg erfahren (s. Brief von Johann II Bernoulli an Leonhard Euler von 1733.08.21). Der in Z\xfcrich lebende Adressat dieses Briefes kann also nicht Johann Jakob Scheuchzer sein. Der Adressat ist daher h\xf6chst wahrscheinlich dessen Bruder Johannes Scheuchzer.')
+
+
+
