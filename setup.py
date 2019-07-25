364839fcabd0210f8265347ecbeee947b0e472c5
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2019-07-25 08:39:31 +0200

    feature (version 1.0.0): make version 1.0.0. (#27)

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="alephmarcreader",
-    version="0.0.1",
+    version="1.0.0",
     author="Tobias Schweizer, Digital Humanities Lab, University of Basel; Balduin Landolt, University of Basel",
     author_email="t.schweizer@unibas.ch",
-    description="A package to read Marc files obtained from Aleph, catalogue of the library of the University of Basel",
+    description="A package to read Marc data obtained from Aleph, the catalogue of the library of the University of Basel",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dhlab-basel/alephmarcreader.git",
-    packages=setuptools.find_packages(exclude=("alephmarc21reader.tests",)),
+    packages=setuptools.find_packages(exclude=("alephmarcreader.tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
